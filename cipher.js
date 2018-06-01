9dcc3c48728f74bafcf8f8371d5550cc1254c5a4
Alicia Centeno <alicent.08.02@gmail.com>
2018-06-01 00:11:20 -0400

    se agrega mas codigo

--- cipher.js
+++ cipher.js
@@ -1,11 +1,27 @@
 window.cipher = {
+  // funcion flecha//
   encode: (sentence,number) => {
-    let sentenceCode,i,result = "";
+    const sentenceCode = "";
+    const i = "";
+    const result = "";
 sentence = sentence.toUpperCase();
 number = parseInt(number);
 console.log(sentence);
-  }
+
+for (const i = 0; i < sentence.length; i++) {
+      if (sentence.charCodeAt(i) === 32){
+        result += "";
+      }
+      else{
+        wordToCode = ((sentence.charCodeAt(i) - 65 + espacios)) % 26 + 65;
+        result += String.fromCharCode(wordToCode);
+      }
+  
+
+  
+}
+}
   decode: () => {
-    
-  }
+      /* Acá va tu código */
+    }
 }
