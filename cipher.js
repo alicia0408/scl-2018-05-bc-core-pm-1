053edeb0a61be8620db96182a25007636bce12a7
Alicia Centeno <alicent.08.02@gmail.com>
2018-06-01 11:21:50 -0400

    se agrega codigo para el cifrado cesar sin exito

--- cipher.js
+++ cipher.js
@@ -1,27 +1,35 @@
 window.cipher = {
-  // funcion flecha//
-  encode: (sentence,number) => {
-    const sentenceCode = "";
-    const i = "";
-    const result = "";
-sentence = sentence.toUpperCase();// la oracion se pasa a mayuscula//
-number = parseInt(number);
-console.log(sentence);
-
-for (const i = 0; i < sentence.length; i++) {
-      if (sentence.charCodeAt(i) === 32){
-        result += "";
+  encode: (oracion, posicion) => {/* funcion o metodo del objeto cipher, funcion flecha*/
+    let i, oracionCode, result = "";
+    oracion = oracion.toUpperCase();//pasamos la frase a mayusculas
+    posicion = parseInt(posicion);
+    
+    for (i = 0; i < oracion.length; i++){
+      if (oracion.charCodeAt(i) === 32) {
+        result += ' ';
       }
-      else{
-        wordToCode = ((sentence.charCodeAt(i) - 65 + espacios)) % 26 + 65;//formula para ejecutar el cifrado//
-        result += String.fromCharCode(wordToCode);//variable que contiene el resultado de la oracion cifrada//
+      else {
+        oracionCode = ((oracion.charCodeAt(i) - 65 + posicion)) % 26 + 65;//ciframos segun la formula
+        result += String.fromCharCode(oracionCode);//llenamos la variable con los caracteres ya cifrados
       }
-  console.log(result);
-}
-result()
-},
- 
-decode: () => {
-      /* Acá va tu código */
-    }
+    } //cierre del for
+    return result;
+  }, //cierre funcion encode
+  
+  decode: (oracion, posicion) => {
+    let result = "", i, oracionCode;
+    oracion = oracion.toUpperCase();//pasamos la frase a mayusculas
+    espacios = parseInt(posicion);
+    
+    for (i = 0; i < oracion.length; i++){
+      if (oracion.charCodeAt(i) === 32){
+        result += ' ';
+      }
+      else {
+        oracionCode = (oracion.charCodeAt(i) + 65 - (posicion)) % 26 + 65;//deciframos segun la formula
+        result += String.fromCharCode(oracionCode);
+      }
+    }  
+    return result;
+  }
 }
