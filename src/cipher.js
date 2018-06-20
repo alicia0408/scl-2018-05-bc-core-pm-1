398901d824b70b834b4e883638992ea9f4cb7448
Alicia Centeno <alicent.08.02@gmail.com>
2018-06-20 10:36:17 -0400

    se hace cambios en variables

--- src/cipher.js
+++ src/cipher.js
@@ -1,36 +1,36 @@
 window.cipher = { // conexion con el evento
   encode: (oracion, posicion) => {// funcion flecha 
     let result = "";
-
-   oracion = oracion.toUpperCase();//pasamos la frase a mayusculas
+    let workToCode = 0;
+  
+    oracion = oracion.toUpperCase();//pasamos la frase a mayusculas
     posicion = parseInt(posicion);
     for ( let i = 0; i < oracion.length; i++) {
       if (oracion.charCodeAt(i) === 32) {
         result += ' ';
-      }
-      else {
-        let workToCode = ((oracion.charCodeAt(i) - 65 + posicion)) % 26 + 65;//se cifra segun la formula
+      
+      }else {
+         workToCode = ((oracion.charCodeAt(i) - 65 + posicion)) % 26 + 65;//se cifra segun la formula
         result += String.fromCharCode(workToCode);//procedemos a que el dato cifrado se almecene en la variable.
       }
     }
     return result;
   },
 
-
-  decode: (oracion, posicion) => {
+ decode: (oracion, posicion) => {
      let result = "";
-   
-     oracion = oracion.toUpperCase();//pasamos la frase a mayusculas
+   let workToCode = 0;
+     
+   oracion = oracion.toUpperCase();//pasamos la frase a mayusculas
      posicion = parseInt(posicion);
        for ( let i = 0; i < oracion.length; i++) {
           if (oracion.charCodeAt(i) === 32) {
             result += ' ';
-      }
-      else {
-       let workToCode = (oracion.charCodeAt(i) + 65 - (posicion)) % 26 + 65;//deciframos segun la formula
+      }else {
+         workToCode = (oracion.charCodeAt(i) + 65 - (posicion)) % 26 + 65;//deciframos segun la formula
         result += String.fromCharCode(workToCode);
       }
     }
     return result;
-  }//cierre de decode
+  },//cierre de decode
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 window.onload = function () {
-
+	
 	const botonCodifica = document.getElementById("codifica"); // variable que trae el valor del boton codifica true or false
 	const botonDecodifica = document.getElementById("decodifica"); // variable que trae el valor del boton decodifica true or false
-
-	// se crea el evento por medio de addEvenlistener por medio del clik
+// se crea el evento por medio de addEvenlistener por medio del clik
 	botonCodifica.addEventListener('click', function () { // Evento que se hace por medio del click
 		let texto = "";
 		texto = document.getElementById("oracion").value; //guardo en una variable el valor que el usuario ingresó en la caja.
